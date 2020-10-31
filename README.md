93d35dd64e63c9d81e0ea720f85759e877c0c56b
Yann Debray <ydebray@mathworks.com>
2020-10-31 19:40:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Spotify music recommendation
+
+![feature-plot](spotifyData/features.svg)
+
+
+## Spotipy
+
+https://spotipy.readthedocs.io/
+
+### Current user
+
+### Saved Tracks
+
+### Saved Albums
+https://developer.spotify.com/console/get-current-user-saved-albums
+
+### Playlist
+https://developer.spotify.com/console/get-current-user-playlists/
+
+#### Add tracks to playlist
+
+### Recently played
+https://developer.spotify.com/console/get-recently-played/
+
+### Artist albums
+
+### Artist top tracks
+
+### Search
+
+https://medium.com/@maxtingle/getting-started-with-spotifys-api-spotipy-197c3dc6353b
+
+### Get token
+chain the login commands in the function get_token:
+* https://dev.to/mxdws/using-python-with-the-spotify-api-1d02
+* https://medium.com/analytics-vidhya/build-your-own-playlist-generator-with-spotifys-api-in-python-ceb883938ce4
+* https://towardsdatascience.com/get-your-spotify-streaming-history-with-python-d5a208bbcbd3
+
+### Get features
+
+### Get recommendations based on seeds
+https://developer.spotify.com/console/get-recommendations/
+
+## Parse Streaming History
+
+## Music Taste Analysis
+* https://towardsdatascience.com/a-music-taste-analysis-using-spotify-api-and-python-e52d186db5fc
+* https://github.com/jmcabreira/A-Music-Taste-Analysis-Using-Spotify-API-and-Python.
+
+
+## Resources
+* https://dev.to/mxdws/using-python-with-the-spotify-api-1d02
+* https://medium.com/python-in-plain-english/music-recommendation-system-for-djs-d253d472677e
+* https://github.com/tgel0/spotify-data
+* https://towardsdatascience.com/a-visual-look-at-my-taste-in-music-a8c197a728be
+
---
+++ spotifyAPI.py
@@ -0,0 +1,144 @@
+import base64
+import requests
+import json
+
+def get_token(clientId,clientSecret):
+    url = "https://accounts.spotify.com/api/token"
+    headers = {}
+    data = {}
+    message = f"{clientId}:{clientSecret}"
+    messageBytes = message.encode('ascii')
+    base64Bytes = base64.b64encode(messageBytes)
+    base64Message = base64Bytes.decode('ascii')
+    headers['Authorization'] = "Basic " + base64Message
+    data['grant_type'] = "client_credentials"
+    r = requests.post(url, headers=headers, data=data)
+    token = r.json()['access_token']
+    return token
+
+
+def get_track_id(track_name: str, token: str) -> str:
+    headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer ' + token,
+    }
+    params = [
+    ('q', track_name),
+    ('type', 'track'),
+    ]
+    try:
+        response = requests.get('https://api.spotify.com/v1/search', 
+                    headers = headers, params = params, timeout = 5)
+        json = response.json()
+        first_result = json['tracks']['items'][0]
+        track_id = first_result['id']
+        return track_id
+    except:
+        return None
+
+
+def get_track_id2(track_name: str, token: str, 
+                artist: str = None) -> str:
+    
+    '''Performs a query on Spotify API to get a track ID.
+    See https://curl.trillworks.com/'''
+   
+    headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer ' + token,
+    }
+    
+    params = [
+    ('q', track_name),
+    ('type', 'track'),
+    ]
+    
+    if artist: 
+        params.append(('artist', artist))
+        
+    try:
+        response = requests.get('https://api.spotify.com/v1/search', 
+                    headers = headers, params = params, timeout = 5)
+        json = response.json()
+        first_result = json['tracks']['items'][0]
+        track_id = first_result['id']
+        return track_id
+    except:
+        return None
+
+def get_track_reco(seed_tracks,token):
+    limit = 10
+    # seed_tracks = "0c6xIDDpzE81m2q797ordA"
+    recUrl = f"https://api.spotify.com/v1/recommendations?limit={limit}&seed_tracks={seed_tracks}"
+
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+
+    res = requests.get(url=recUrl, headers=headers)
+    return res.json()
+
+#Import Libraries for Feature plot
+import numpy as np
+import matplotlib.pyplot as plt
+
+def feature_plot(features):
+        
+    labels= list(features)[:]
+    stats= features.mean().tolist()
+
+    angles=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+
+    # close the plot
+    stats=np.concatenate((stats,[stats[0]]))
+    angles=np.concatenate((angles,[angles[0]]))
+
+    #Size of the figure
+    fig=plt.figure(figsize = (18,18))
+
+    ax = fig.add_subplot(221, polar=True)
+    ax.plot(angles, stats, 'o-', linewidth=2, label = "Features", color= 'gray')
+    ax.fill(angles, stats, alpha=0.25, facecolor='gray')
+    ax.set_thetagrids(angles[0:7] * 180/np.pi, labels , fontsize = 13)
+
+
+    ax.set_rlabel_position(250)
+    plt.yticks([0.2 , 0.4 , 0.6 , 0.8  ], ["0.2",'0.4', "0.6", "0.8"], color="grey", size=12)
+    plt.ylim(0,1)
+
+    plt.legend(loc='best', bbox_to_anchor=(0.1, 0.1))
+
+
+def feature_plot2(features1,features2):
+    labels= list(features1)[:]
+    stats= features1.mean().tolist()
+    stats2 = features2.mean().tolist()
+
+    angles=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+
+    # close the plot
+    stats=np.concatenate((stats,[stats[0]]))
+    stats2 =np.concatenate((stats2,[stats2[0]])) 
+    angles=np.concatenate((angles,[angles[0]]))
+
+    #Size of the figure
+    fig=plt.figure(figsize = (18,18))
+
+    ax = fig.add_subplot(221, polar=True)
+    ax.plot(angles, stats, 'o-', linewidth=2, label = "Features 1", color= 'gray')
+    ax.fill(angles, stats, alpha=0.25, facecolor='gray')
+    ax.set_thetagrids(angles * 180/np.pi, labels , fontsize = 13)
+
+
+    ax.set_rlabel_position(250)
+    plt.yticks([0.2 , 0.4 , 0.6 , 0.8  ], ["0.2",'0.4', "0.6", "0.8"], color="grey", size=12)
+    plt.ylim(0,1)
+
+    ax.plot(angles, stats2, 'o-', linewidth=2, label = "Features 2", color = 'm')
+    ax.fill(angles, stats2, alpha=0.25, facecolor='m' )
+    ax.set_title('Mean Values of the audio features')
+    ax.grid(True)
+
+    plt.legend(loc='best', bbox_to_anchor=(0.1, 0.1))
