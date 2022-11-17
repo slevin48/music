0cdc10de34930da46c037102d2ecf01921f11707
Slevin <yann.debray@gadz.org>
2022-11-16 22:27:57 -0500

    Update authentication for Streamlit Cloud

--- code/s3_app.py
+++ code/s3_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import boto3
 
+s3_client = boto3.client('s3',aws_access_key_id = st.secrets["aws"]["aws_access_key_id"],
+                    aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"])
+
 def s3download(music):
-    s3_client = boto3.client('s3')
     s3_client.download_file("music48", music, music)
 
 st.set_page_config(page_title="S3 Music",page_icon="🎵")
