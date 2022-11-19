1e13198074d8cc6c820e56a63bf4a013fcc0ebaa
Slevin <yann.debray@gadz.org>
2022-11-19 18:47:53 -0500

    Download music from S3

--- code/s3_app.py
+++ code/s3_app.py
@@ -1,24 +1,81 @@
 import streamlit as st
-import boto3, os
+import boto3, os, s3fs
+from zipfile import ZipFile
+
+# Create connection object.
+fs = s3fs.S3FileSystem(key = st.secrets["aws"]["aws_access_key_id"],
+                        secret = st.secrets["aws"]["aws_secret_access_key"])
 
 try:
     os.mkdir('downloads')
 except OSError as error:
-    print(error)
-
-s3_client = boto3.client('s3',aws_access_key_id = st.secrets["aws"]["aws_access_key_id"],
-                    aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"])
-
-def s3download(music):
-    s3_client.download_file("music48", music, music)
+    # print(error)
+    pass
 
 st.set_page_config(page_title="S3 Music",page_icon="🎵")
+
+@st.experimental_memo(ttl=600)
+def read_file(filename):
+    with fs.open(filename,"rb") as f:
+        return f.read()
+
+def write_local(filename,content):
+    with open("downloads/"+filename,"wb") as f:
+        f.write(content)
+
+# l = fs.listdir("music48")
+l = [i['Key'] for i in fs.listdir("music48")]
+# l = [i.replace('music48/','') for i in fs.ls('music48')]
+# st.write(l)
+p = st.sidebar.selectbox("Select Playlist",l,format_func = lambda x : x.replace("music48/",""))
+# st.sidebar.write(fs.listdir(p))
+# st.sidebar.write(fs.isdir(p))
+
 st.title("S3 Music 🎵")
 
-f = [key['Key'] for key in s3_client.list_objects(Bucket='music48',Prefix="downloads/")['Contents']]
-m = st.selectbox("Select Music",f,
-            format_func = lambda x : x.replace("downloads/","").replace(".mp3",""))
+d={}
+b = st.checkbox("Select All")
+st.write("---")
 
-s3download(m)
+for i in fs.listdir(p):
+    if i['type'] == "directory":
+        st.write(i['Key'].replace(p,"")) 
+        # TODO: add expander https://docs.streamlit.io/library/api-reference/layout/st.expander
+        for j in fs.listdir(i['Key']):
+            d[j['Key']] = st.checkbox(j['Key'].replace(i['Key']+"/",""),value=b)
+            # st.write(j['Key'])
+    elif i['type'] == "file":
+        d[i['Key']] = st.checkbox(i['Key'].replace(p+"/",""),value=b)
 
-st.audio(m)
+# for i in fs.ls(p):
+#     d[i] = st.checkbox(i,value=b)
+
+# st.write(d)
+
+# for key in d:
+#     if d[key]:
+#         st.write(key)
+
+t = [key for key in d if d[key]]
+# st.write(t)
+z = ZipFile('downloads.zip', 'w')
+for e in t:
+    try:
+        c = read_file(e)
+        f = e.replace("music48/","").replace("/"," - ")
+        # st.write(f)
+        # st.audio(c)
+        write_local(f,c)
+        z.write('downloads/'+f)
+    except: # avoid PermissionError: The operation is not valid for the object's storage class
+        pass
+
+z.close()
+
+with open("downloads.zip", "rb") as fp:
+    btn = st.download_button(
+        label="Download ZIP",
+        data=fp,
+        file_name="music.zip",
+        mime="application/zip"
+    )
---
+++ code/s3basic_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import boto3, os
+
+try:
+    os.mkdir('downloads')
+except OSError as error:
+    print(error)
+
+s3_client = boto3.client('s3',aws_access_key_id = st.secrets["aws"]["aws_access_key_id"],
+                    aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"])
+
+def s3download(music):
+    s3_client.download_file("music48", music, music)
+
+st.set_page_config(page_title="S3 Music",page_icon="🎵")
+st.title("S3 Music 🎵")
+
+f = [key['Key'] for key in s3_client.list_objects(Bucket='music48',Prefix="downloads/")['Contents']]
+m = st.selectbox("Select Music",f,
+            format_func = lambda x : x.replace("downloads/","").replace(".mp3",""))
+
+s3download(m)
+
+st.audio(m)
